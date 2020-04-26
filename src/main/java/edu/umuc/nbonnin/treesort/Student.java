49349a88b13f43e91b2fed5eb13d01f8ce1af557
Naomi Bonnin <nbonnin@student.umuc.edu>
2020-04-26 15:51:40 -0400

    Student done
    Signed-off-by: Naomi Bonnin <nbonnin@student.umuc.edu>

--- src/main/java/edu/umuc/nbonnin/treesort/Student.java
+++ src/main/java/edu/umuc/nbonnin/treesort/Student.java
@@ -1,11 +1,17 @@
 package edu.umuc.nbonnin.treesort;
 
+/*
+ * Skeleton Class as a proof of concept
+ * No real comments as it isn't really graded
+ */
 public class Student {
 
+    //Instance variables
     private String lastName, firstName;
     private int gradeLevel, studentID;
     private double gpa;
 
+    //Constructors to assign instance variables
     public Student(
             String lastName,
             String firstName,
@@ -20,10 +26,12 @@ public class Student {
         this.gpa = gpa;
     }
 
+    //Overridden tostring method
     public String toString() {
         return firstName + " " + lastName;
     }
 
+    //Below are all of the getters and setters
     public String getLastName() {
         return lastName;
     }
